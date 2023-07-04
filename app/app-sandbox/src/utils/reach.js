7313823e6593694d28531e9f3fa1eeeb74ebadbf
Nicholas Shellabarger <temptemp3@users.noreply.github.com>
2023-07-03 22:07:35 -0600

    override ALGO_MakePeraConnect

--- app/app-sandbox/src/utils/reach.js
+++ app/app-sandbox/src/utils/reach.js
@@ -5,11 +5,68 @@ import QRCodeModal from "algorand-walletconnect-qrcode-modal";
 
 import MyAlgoConnect from "@randlabs/myalgo-connect";
 
-import { ALGO_MakePeraConnect as MakePeraConnect } from "@reach-sh/stdlib";
 import { PeraWalletConnect } from "@perawallet/connect";
+
+import { decodeUnsignedTransaction } from "algosdk";
+
+// override
+function ALGO_MakePeraConnect(PeraWalletConnect) {
+  return class PeraConnect {
+    constructor(pc = false) {
+      this.pc = pc;
+      this.accounts = [];
+    }
+    async ensurePC() {
+      if (this.pc) {
+        return;
+      }
+      this.pc = new PeraWalletConnect();
+      this.pc.reconnectSession().then((accts) => {
+        this.accounts = accts;
+      });
+    }
+    async disconnect() {
+      this.pc.disconnect();
+      this.accounts = [];
+    }
+    async ensureSession() {
+      await this.ensurePC();
+      if (this.accounts.length === 0) {
+        this.accounts = await this.pc.connect();
+      }
+    }
+    async getAddr() {
+      await this.ensureSession();
+      return this.accounts[0];
+    }
+    async signTxns(txns) {
+      let rawSignedTxns;
+      await this.ensureSession();
+      rawSignedTxns = await this.pc.signTransaction([
+        txns.map((value) => {
+          const decodedUnsignedTxn = decodeUnsignedTransaction(
+            Buffer.from(value, "base64")
+          ); 
+          return {
+            txn: decodedUnsignedTxn,
+          };
+        }),
+      ]);
+      return rawSignedTxns.map(
+        (value) => Buffer.from(value).toString("base64") 
+      );
+    }
+  };
+}
 
 export const makeStdLib = () => {
   const wallet = JSON.parse(localStorage.getItem("txnlab-use-wallet") || "{}");
+  const baseProviderId = wallet?.state?.activeAccount?.providerId;
+  const providedId =
+    baseProviderId === "pera"
+      ? JSON.parse(localStorage.getItem("PeraWallet.Wallet") || "{}")?.type
+      : baseProviderId;
+
   const [node, customNode, customIndexer] = (
     localStorage.getItem("node") || "algorand-testnet::"
   ).split(":");
@@ -50,23 +107,17 @@ export const makeStdLib = () => {
     ALGO_INDEXER_SERVER,
     ALGO_INDEXER_PORT: "",
   };
-  switch (wallet?.state?.activeAccount?.providerId) {
-    // TODO fix issue introduced afte upgrading pera wallet
-    // (1) options bump down pera wallet
-    // (2) use wallet connect for pera wallet
-    // until the issue is isolated and fixed
-    // See https://github.com/reach-sh/reach-lang/issues/1548
-    // See also https://github.com/temptemp3/arc-200/issues/29
-    /*
-    case "pera":
+
+  switch (providedId) {
+    case "pera-wallet":
+    case "pera-wallet-web":
       stdlib.setWalletFallback(
         stdlib.walletFallback({
           providerEnv,
-          WalletConnect: MakePeraConnect(PeraWalletConnect),
+          WalletConnect: ALGO_MakePeraConnect(PeraWalletConnect),
         })
       );
       break;
-    */
     case "myalgo":
       stdlib.setWalletFallback(
         stdlib.walletFallback({
@@ -77,7 +128,6 @@ export const makeStdLib = () => {
       break;
     // case daffi sorta works
     // case defly
-    case "pera":
     default:
       stdlib.setWalletFallback(
         stdlib.walletFallback({
